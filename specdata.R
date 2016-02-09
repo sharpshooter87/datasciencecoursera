11f5b987ad5a9851230b5825aa8d3358772c2e52
sharpshooter87 <pushkarghatole@gmail.com>
2016-02-09 09:40:38 +0530

    Update specdata.R

--- specdata.R
+++ specdata.R
@@ -33,3 +33,11 @@ pollutantmean<-function(directory,pollutant,id=1:332){
         polmean       
         
 }
+
+#Results
+#> pollutantmean("specdata", "sulfate", 1:10)
+#[1] 4.064
+#> pollutantmean("specdata", "nitrate", 70:72)
+#[1] 1.706
+#> pollutantmean("specdata", "nitrate", 23)
+#[1] 1.281
